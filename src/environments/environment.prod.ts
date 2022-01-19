741d39ba445a34f9be44766a287dbe9b418870ea
Valéria do Valle <valeriaabvalle@gmail.com>
2022-01-18 21:39:20 -0300

    Configurações iniciais, Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'filmflix-576cc',
+    appId: '1:733433679384:web:9f9933a7ce7db84129bd6d',
+    storageBucket: 'filmflix-576cc.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDz-Lx73QjVcHVRqL3gDQ5jAeIMvVnPDLg',
+    authDomain: 'filmflix-576cc.firebaseapp.com',
+    messagingSenderId: '733433679384',
+  },
   production: true
 };
